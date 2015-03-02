4b922f6e2fc4b7bd22480612092f09ffe96df5cc
Benedict Chan <purple.enchanted@gmail.com>
2015-03-02 15:37:25 +1100

    Try adding the {base:} as an argument

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -3,12 +3,12 @@ var concat = require('gulp-concat');
 var del = require('del');
 
 
-var source_root = 'source';
+var source_root = 'source/';
 var paths = {
 	scripts: [
-		source_root + '\\js\\' + 'jquery.waypoints.min.js',
-		source_root + '\\js\\' +'bootstrap.min.js',
-		source_root + '\\js\\' +'d3.min.js',
+		source_root + 'js/jquery.waypoints.min.js',
+		source_root + 'js/bootstrap.min.js',
+		source_root + 'js/d3.min.js',
 		]
 };
 
@@ -20,7 +20,7 @@ gulp.task('clean:scripts', function(cb){
 });
 
 gulp.task('concat:scripts', ['clean:scripts'], function(){
-	return gulp.src(paths.scripts)
+	return gulp.src(paths.scripts, {base: source_root})
 	.pipe(concat('all.js'))
 	.pipe(gulp.dest('./build/js'));
 });
