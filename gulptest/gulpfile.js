1566846bd2ffd0b5ebe20cada10d1944ae351418
Benedict Chan <purple.enchanted@gmail.com>
2015-03-04 15:32:02 +1100

    Added gulp-watch, and a tast to test whether the watch is working

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -4,6 +4,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
 var rename = require('gulp-rename');
+var watch = require('gulp-watch');
 
 var del = require('del');
 
@@ -22,13 +23,19 @@ var paths = {
 };
 
 
-gulp.task('clean', function(cb){
+gulp.task('clean:js', function(cb){
 	del([
-		'build'
+		'build/js/**'
 		], cb);
 });
 
-gulp.task('concat:scripts', ['clean'], function(){
+gulp.task('clean:css', function(cb){
+	del([
+		'build/*.css'
+		], cb);
+});
+
+gulp.task('concat:scripts', ['clean:js'], function(){
 	return gulp.src(paths.scripts, {base: source_root})
 	.pipe(concat('all.js'))
 	.pipe(gulp.dest('./build/js'))
@@ -39,7 +46,7 @@ gulp.task('concat:scripts', ['clean'], function(){
 });
 
 
-gulp.task('concat:styles', ['clean'], function(){
+gulp.task('concat:css', ['clean:css'], function(){
 	return gulp.src(paths.styles, {base: source_root})
 	.pipe(concat('style.css'))
 	.pipe(gulp.dest('./build/'))
@@ -50,4 +57,12 @@ gulp.task('concat:styles', ['clean'], function(){
 });
 
 
-gulp.task('default', ['concat:scripts', 'concat:styles']);
+gulp.task('watch:css', function(){
+	gulp.watch(source_root + '**/*.css', ['concat:css']);
+});
+
+
+gulp.task('yo', function(){console.info('yo');});
+
+gulp.task('default', ['concat:scripts', 'concat:css', 'watch:css']);
+
