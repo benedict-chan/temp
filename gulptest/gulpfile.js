aa62a3ff8873002aaff543f3cd6581a06e9a9f62
Benedict Chan <purple.enchanted@gmail.com>
2015-03-12 17:20:22 +1100

    Modified as to be a draft of merging some wordpress styles

--- gulptest/gulpfile.js
+++ gulptest/gulpfile.js
@@ -9,7 +9,7 @@ var watch = require('gulp-watch');
 var del = require('del');
 
 
-var source_root = 'source/';
+var source_root = 'C:\\TZLocalProjects\\TZ.Website\\Trunk\\Source\\revamp.tz.net\\htdocs\\wp-content/';
 var dest_root = 'build/';
 
 var paths = {
@@ -18,10 +18,27 @@ var paths = {
 		source_root + 'js/bootstrap.min.js',
 		source_root + 'js/d3.min.js',
 		],
-	styles: [
-		source_root + 'css/animate.min.css',
-		source_root + 'css/bootstrap.min.css',
-		]
+	styles_noncustom: [
+		source_root + 'plugins/aqua-page-builder/assets/stylesheets/aqpb-view.css',
+		source_root + 'plugins/purethemes-shortcodes/css/shortcodes.css',
+		source_root + 'plugins/contact-form-7/includes/css/styles.css',
+
+		source_root + 'themes/tz-astrum/css/bootstrap.min.css',
+		source_root + 'themes/tz-astrum/css/animate.min.css',
+
+		source_root + 'themes/astrum-theme/css/base.css',
+		source_root + 'themes/astrum-theme/css/responsive.css',
+		source_root + 'themes/astrum-theme/css/icons.css',
+		// source_root + 'themes/astrum-theme/style.css',
+
+		source_root + 'themes/astrum-theme/css/woocommerce.css',
+
+
+		],
+	styles_custom:[
+		source_root + 'themes/astrum-theme/style.css',
+		source_root + 'themes/tz-astrum/style.css',
+	],
 };
 
 
@@ -33,6 +50,7 @@ gulp.task('clean:js', function(cb){
 
 gulp.task('clean:css', function(cb){
 	del([
+		dest_root + 'css/**',
 		dest_root + '*.css'
 		], cb);
 });
@@ -48,23 +66,34 @@ gulp.task('concat:scripts', ['clean:js'], function(){
 });
 
 
-gulp.task('concat:css', ['clean:css'], function(){
-	return gulp.src(paths.styles, {base: source_root})
-	.pipe(concat('style.css'))
+gulp.task('concat-noncustom:css', ['clean:css'], function(){
+	return gulp.src(paths.styles_noncustom, {base: source_root})
+	.pipe(concat('noncustom.css'))
 	.pipe(gulp.dest(dest_root  + '/css'))
 	.pipe(minifyCSS())
-	.pipe(rename('style.min.css'))
+	.pipe(rename('noncustom.min.css'))
 	.pipe(gulp.dest(dest_root + '/css'))
 	.on('error', gutil.log);
 });
 
+gulp.task('concat-custom:css', [], function(){
+	return gulp.src(paths.styles_custom, {base: source_root})
+	.pipe(concat('custom.css'))
+	.pipe(gulp.dest(dest_root ))
+	.pipe(minifyCSS())
+	.pipe(rename('custom.min.css'))
+	.pipe(gulp.dest(dest_root))
+	.on('error', gutil.log);
+});
+
+
 
 gulp.task('watch:css', function(){
-	gulp.watch(source_root + '**/*.css', ['concat:css']);
+	gulp.watch(source_root + '**/*.css', ['concat-noncustom:css']);
 });
 
 
 gulp.task('yo', function(){console.info('yo');});
 
-gulp.task('default', ['concat:scripts', 'concat:css']);
+gulp.task('default', ['concat-noncustom:css', 'concat-custom:css']);
 
