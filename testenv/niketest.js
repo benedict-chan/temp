b5556311928df59d6fc79521157ce119974c08f5
Benedict Chan <purple.enchanted@gmail.com>
2015-07-20 16:51:18 +1000

    just some test script

--- testenv/niketest.js
+++ testenv/niketest.js
@@ -1,88 +1,41 @@
-function commonAddToCart(d, g, f, c, i, h, b, e) {
-    if (g == 0) {
-        g = 1
-    }
-    if (!isNullOrEmpty(d) && g > 0) {
-        var a = _contextPath + "/shoppingcart/add";
-        $j.post(a, {upc: d,count: g}, function(j) {
-            if (j.shoppingCartSummary != null) {
-                var m = j.shoppingCartSummary.shoppingCartSize;
-                var n = j.shoppingCommand;
-                parseInt(m) != 0 && $j(".buycart-num-wrap").removeClass("none");
-                if (m > 0) {
-                    $j(".cartKazu").show()
-                }
-                $j("#buycart-count").html(m);
-                if (!b) {
-                    var k = "";
-                    if (e) {
-                        k = $j("#quickbuyDialog #relatedColors .selected img").attr("curSrc")
-                    } else {
-                        k = $j("#relatedColors .selected img").attr("curSrc")
-                    }
-                    showCartAddedItemWithHeader(k, i, h, n.price, n.sizeRef, n.quantity, n.skuColor, j.shoppingCartSummary.totalAfDiscount);
-                    if (e) {
-                        var l = $j("#pdpRecommend").val();
-                        if (l == "true") {
-                            omnitureRecommendQuickBuyAddToCart(c, f)
-                        } else {
-                            omnitureQuickBuyAddToCart(c, f)
-                        }
-                        $j(".black_opacity").fadeOut(300);
-                        $j("#quickbuyDialog").fadeOut(300);
-                        try {
-                            if (s.oldPageName) {
-                                s.pageName = s.oldPageName
-                            }
-                            if (oldP3) {
-                                p3 = oldP3
-                            }
-                            if (oldP17) {
-                                p17 = oldP17
-                            }
-                        } catch (o) {
-                        }
-                    } else {
-                        if (pdpRecommend) {
-                            omnitureRecommendCartAddedHandler(c, f)
-                        } else {
-                            omnitureCartAddedHandler(c, f)
-                        }
-                    }
-                } else {
-                    if (e) {
-                        var l = $j("#pdpRecommend").val();
-                        if (l == "true") {
-                            omnitureRecommendQuickBuyAddToCart(c, f)
-                        } else {
-                            omnitureQuickBuyAddToCart(c, f)
-                        }
-                    } else {
-                        if (pdpRecommend) {
-                            omnitureRecommendCartAddedHandler(c, f)
-                        } else {
-                            omnitureCartAddedHandler(c, f)
-                        }
-                    }
-                    window.location.href = _contextPath + "/shoppingcart"
-                }
-            } else {
-                if (j.backWarnEntity.description != null) {
-                    $j("#quickbuyDialog").hide();
-                    sAlert(j.backWarnEntity.description)
-                } else {
-                    $j("#quickbuyDialog").hide();
-                    sAlert(loxia.getLocaleMsg("mynike.edit.myfavorite.AddFailure"))
-                }
-            }
-        })
-    } else {
-        if (e) {
-            $j("#quickbuyDialog").find("#pdpSizeError").html(loxia.getLocaleMsg("quickBuy.SelectSize") + "<i></i>").show();
-            $j("#quickbuyDialog").find("#pdpSizeErrorColor").addClass("error")
-        } else {
-            $j("#pdpSizeError").html(loxia.getLocaleMsg("quickBuy.SelectSize") + "<i></i>").show();
-            $j("#pdpSizeErrorColor").addClass("error")
-        }
-    }
+$j.post("/shoppingcart/add", {upc: "00659658169586",count: 1}, function(j) {
+    var n = j.shoppingCommand;
+    console.dir(n);
+    omnitureCartAddedHandler("768929-623", "1299");
+});
+
+$j.post("/shoppingcart/add", {upc: "00659658169586",count: 1}, function(j) {
+    var n = j.shoppingCommand;
+    console.dir(n);
+});
+
+768861-601 site:www.nikestore.com.hk
+
+http://www.nikestore.com.hk/product/768929-623/detail.htm?pdpRecommend=false&preSkuCode=
+http://www.nikestore.com.hk/product/768861-601/detail.htm?pdpRecommend=false&preSkuCode=
+omnitureQuickBuyAddToCart("768861-601", "1249")
+
+loxia.syncXhrPost( "/product/validateSku", {skuCode: "768861-601"});
+            var e = _contextPath + "/product/" + d + "/dialog";
+            var b = loxia.syncXhrPost("/product/768861-601/dialog", { isSameSku: false,pdpRecommend: true});
+
+
+    var f = loxia.syncXhrPost("/transaction/doValidatorQsGoToTransactionCheck2", {upc: g,count: 1});
+
+
+c = "768929-623", a = "1299"
+function omnitureCartAddedHandler(c, a) {
+    var b = {eVar28: p3 + ":Add to cart",events: "scAdd,event22=" + a,products: ";" + c + ";;",prop3: p3 + ":Add to cart",prop17: p17};
+    trackLink(b, "cartAddedSuccess")
 }
+
+eVar28: "PDP:Add to cart"
+eVar70: "D=c5"
+events: "scAdd,event22=1299"
+products: ";768929-623;;"
+prop3: "PDP:Add to cart"
+prop15: "hk_cn"
+prop17: "PDP"
+
+
+00659658169593
